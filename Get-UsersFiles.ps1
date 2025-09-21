9c9e3f662f9e04f9fedd2f3d1b773697f4efae14
monasibmasr49-ship-it <monasibmasr49@gmail.com>
2025-09-21 18:25:19 +0300

    Add files via upload

---
+++ Get-UsersFiles.ps1
@@ -0,0 +1,10 @@
+﻿#$Directory=Write-Output "Please write your system directory : "
+ #Set-Location -Path $Directory 
+ $UserName = Read-Host "write the UserName : "
+ Set-LocalUser -Name $UserName
+ Get-ChildItem -Path $UserName
+ #Write-Output "Know I set your user location : "
+ #$fileName = read-Host "write your file name : "
+ #Get-ChildItem -Path .\desktop
+ #Invoke-Item  $fileName
+ #Set-Location .\Desktop
